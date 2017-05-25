cecbbdcc5baa6e6ee61f96534e82c3548d9f4a2f
Ron Kurr <kurr@kurron.org>
2017-05-25 15:15:49 -0400

    Remove AMQP references.

--- src/main/groovy/org/kurron/bare/metal/producer/Application.groovy
+++ src/main/groovy/org/kurron/bare/metal/producer/Application.groovy
@@ -1,9 +1,5 @@
 package org.kurron.bare.metal.producer
 
-import org.springframework.amqp.core.Binding
-import org.springframework.amqp.core.BindingBuilder
-import org.springframework.amqp.core.DirectExchange
-import org.springframework.amqp.core.Queue
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -20,20 +16,5 @@ class Application {
 	@Bean
 	CustomApplicationRunner customApplicationRunner() {
 		new CustomApplicationRunner()
-	}
-
-	@Bean
-	DirectExchange exchange( ApplicationProperties configuration ) {
-		new DirectExchange( configuration.exchange, true, false )
-	}
-
-	@Bean
-	Queue queue( ApplicationProperties configuration ) {
-		new Queue( configuration.queue, true )
-	}
-
-	@Bean
-	Binding binding( ApplicationProperties configuration, Queue queue, DirectExchange exchange ) {
-		BindingBuilder.bind( queue ).to( exchange ).with( configuration.routingKey )
 	}
 }
