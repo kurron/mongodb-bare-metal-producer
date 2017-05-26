253be1e5db03fefb77ba0656500cef2d4a763bc0
Ron Kurr <kurr@kurron.org>
2017-05-26 12:13:25 -0400

    The connection string now uses values described in the driver documentation.

--- run-container.sh
+++ run-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-URI=${1:-mongodb://localhost:27017/bare-metal?safe=true&w=majority&readPreference=nearest&appName=bare-metal-producer}
-MESSAGE_COUNT=${2:-2500}
+URI=${1:-mongodb://experiment:experiment@benchmarking-shard-00-00-annli.mongodb.net:27017,benchmarking-shard-00-01-annli.mongodb.net:27017,benchmarking-shard-00-02-annli.mongodb.net:27017/bare-metal-producer?ssl=true&replicaSet=benchmarking-shard-0&authSource=admin&journal=true&w=1&readConcernLevel=majority&readPreference=secondaryPreferred&appName=bare-metal-producer}
+MESSAGE_COUNT=${2:-1024}
 PAYLOAD_SIZE=${3:-1024}
 
 CMD="docker run \
