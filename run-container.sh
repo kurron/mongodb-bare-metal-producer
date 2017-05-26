dfca61d1a54123b0b8d9d725429ace85ccc66935
Ron Kurr <kurr@kurron.org>
2017-05-26 15:49:10 -0400

    Use settings compatible with Amazon Linux and nohup

--- run-container.sh
+++ run-container.sh
@@ -5,13 +5,11 @@ MESSAGE_COUNT=${2:-1024}
 PAYLOAD_SIZE=${3:-1024}
 
 CMD="docker run \
-            --cpus 1 \
             --env spring_data_mongodb_uri=${URI} \
             --interactive  \
             --name mongodb-producer \
             --network host \
             --rm \
-            --tty \
             kurron/mongodb-bare-metal-producer:latest \
             --number-of-messages=${MESSAGE_COUNT} \
             --payload-size=${PAYLOAD_SIZE}"
